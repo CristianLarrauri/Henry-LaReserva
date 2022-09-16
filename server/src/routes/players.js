2d1721e5ab6dfe3b48d81a16df31e505b540a2cb
Juan Manuel <juanberrazmontyn@hotmail.com>
2022-09-16 14:08:59 -0300

    Remove ignored files

--- server/src/routes/players.js
+++ server/src/routes/players.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
-const { create_players } = require("../utils/utilsPlayers");
-const { Players } = require("../db");
+const { create_players, players_db } = require("../utils/utilsPlayers");
+const { Players, Teams, Tournaments } = require("../db");
 
 const router = Router();
 
@@ -12,7 +12,7 @@ router.post("/", async (req, res) => {
       res.status(400).send("Faltan campos obligatorios");
     }
     if (data.name && data.surname && data.dni) {
-      let players = data_player.find((e) => e.dni === e.dni);
+      let players = data_player.find((e) => e.dni == data.dni);
 
       if (!players) {
         create_players(data);
@@ -23,6 +23,26 @@ router.post("/", async (req, res) => {
     }
   } catch (error) {
     console.log("ROMPO EN RUTA POST DE PLAYERS", error);
+  }
+});
+
+router.get("/", async (req, res) => {
+  const { name, surname, dni } = req.query;
+  let the_players = await players_db();
+
+  if (!name || !surname || !dni) {
+    res.status(200).send(the_players);
+  } else {
+    let name_surname_players = the_players.filter(
+      (p) =>
+        p.name.toLowerCase() === name.toLowerCase &&
+        p.surname.toLowerCase() === surname.toLowerCase
+    );
+    name_surname_players.length
+      ? res.status(200).send(name_surname_players)
+      : res
+          .status(404)
+          .send("No se encontro Jugador con ese nombre y apellido.");
   }
 });
 
--- server/src/utils/utilsPlayers.js
+++ server/src/utils/utilsPlayers.js
@@ -2,17 +2,38 @@ const { Players, Teams, Tournaments } = require("../db");
 
 const create_players = async (data) => {
   try {
-    const { name, surname, dni } = data;
+    const { name, surname, dni, teams } = data;
 
     const new_players = await Players.create({
       name,
       surname,
       dni,
     });
+    const players_teams = await Teams.findAll({
+      where: { name: teams },
+    });
+    new_players.addTeams(players_teams);
+
     return new_players;
   } catch (error) {
     console.log("ROMPO EN UTILISPLAYERS", error);
   }
 };
 
-module.exports = { create_players };
+const players_db = async () => {
+  try {
+    return await Players.findAll({
+      include: {
+        model: Teams,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
+    });
+  } catch (error) {
+    console.log("ERROR EN UTILSGET", error);
+  }
+};
+
+module.exports = { create_players, players_db };
