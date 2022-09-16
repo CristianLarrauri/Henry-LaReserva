563ce1ae162f8fa7cd97376a63d4addfe0466aeb
unknown <mauroalos@hotmail.com>
2022-09-16 18:52:12 -0300

    creacion componente Detail

--- client/src/components/TournamentDetails.jsx
+++ client/src/components/TournamentDetails.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import styles from '../styles/TournamentDetails.module.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../redux/actions"
+import { NavLink } from 'react-router-dom';
 
-export default function TournamentDetails(tournaments) {//Esto lo recibe del componente filtros
-    return(
-        <div className={styles.mainWrapper}>
-            Este es el componente que visualizara las cartas
+const TournamentDetail = (props) => {
+     let {id} = props.match.params
+     const dispatch = useDispatch();
+     React.useEffect(()=>{
+        dispatch(actions.tournamentDetail(id));
+     }, [dispatch]);
+
+     let tournament = useSelector((state) => state.tournamentDetail)
+
+    return (
+        <div>
+            
+            <h2>{`Torneo "${tournament.name}"`}</h2>
+            <img src={tournament.img} alt={tournament.name} />
+            <h4>Categoria: {tournament.category}</h4>
+            <h4>Género: {tournament.genre}</h4>
+            <h4>Fecha de inicio: {tournament.dateInit}</h4>
+            <h4>Finaliza: {tournament.dateFinish}</h4>
+            <h4>{tournament.teams? <ul> {tournament.teams.map((team) => <li>{team}</li>)}</ul> : null}</h4>
+
+            <NavLink to="/home"><button>Volver</button></NavLink>
+           
         </div>
-    );
+    )
 }
+
+
+
+
+
+
+
+export default TournamentDetail
