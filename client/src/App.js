71743348583fc139bebcd0b79805e6db44c95be2
Maxi Giagnorio <maxigiagnorio@gmail.com>
2022-09-18 20:03:39 -0300

    componente allTournaments & fix SearchBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,34 @@
-import './App.css';
-import Landing from './components/Landing';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import TeamInscription from './components/TeamInscription';
-import PlayerInscription from './components/PlayerInscription';
-import CreateTournament from './components/CreateTournament';
+import "./App.css";
+import Landing from "./components/Landing";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import TeamInscription from "./components/TeamInscription";
+import PlayerInscription from "./components/PlayerInscription";
+import CreateTournament from "./components/CreateTournament";
+import AllTournaments from "./components/AllTournaments";
 
-import Error404 from './components/Error404';
+import Error404 from "./components/Error404";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<div>
-				<Switch>
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/home" component={Home} />
-					<Route exact path="/inscription" component={TeamInscription} />
-					<Route
-						exact
-						path="/inscription/players"
-						component={PlayerInscription}
-					/>
-					<Route exact path="/create" component={CreateTournament} />
-					<Route exact path="/404" component={Error404} />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/inscription" component={TeamInscription} />
+          <Route exact path="/tournaments" component={AllTournaments} />
+          <Route
+            exact
+            path="/inscription/players"
+            component={PlayerInscription}
+          />
+          <Route exact path="/create" component={CreateTournament} />
+          <Route exact path="/404" component={Error404} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,45 +1,46 @@
 import {
-	CREATE_TOURNAMENT,
-	GET_ALL_TOURNAMENTS,
-	TOURNAMENT_DETAILS,
-	SEARCH_TOURNAMENTS
-} from '../actions/index.js';
+  CREATE_TOURNAMENT,
+  GET_ALL_TOURNAMENTS,
+  TOURNAMENT_DETAILS,
+  SEARCH_TOURNAMENTS,
+} from "../actions/index.js";
 
 let initialState = {
-	tournaments: [],
-	tournamentDetail: {},
-	teams: [],
-	players: [],
-	users: [],
-	userProfile: {}
+  tournaments: [],
+  tournamentDetail: {},
+  teams: [],
+  players: [],
+  users: [],
+  userProfile: {},
 };
 
 function rootReducer(state = initialState, action) {
-	switch (action.type) {
-		case CREATE_TOURNAMENT:
-			return {
-				...state
-			};
-		case GET_ALL_TOURNAMENTS:
-			console.log('entrando', state.tournaments);
-			return {
-				...state,
-				tournaments: action.payload
-			};
+  switch (action.type) {
+    case CREATE_TOURNAMENT:
+      return {
+        ...state,
+      };
+    case GET_ALL_TOURNAMENTS:
+      console.log("entrando", state.tournaments);
+      return {
+        ...state,
+        tournaments: action.payload,
+      };
 
-		case SEARCH_TOURNAMENTS:
-			return {
-				...state,
-				tournaments: action.payload
-			};
-		case TOURNAMENT_DETAILS:
-			return {
-				...state,
-				tournamentDetail: action.payload
-			};
-		default:
-			return state;
-	}
+    case SEARCH_TOURNAMENTS:
+      return {
+        ...state,
+        tournaments: [action.payload],
+      };
+    case TOURNAMENT_DETAILS:
+      return {
+        ...state,
+        tournamentDetail: action.payload,
+      };
+
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
