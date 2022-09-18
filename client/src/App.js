73628ee9ed83e4b8953e9c77d42b65895b6a76e9
Maxi Giagnorio <maxigiagnorio@gmail.com>
2022-09-18 17:17:00 -0300

    comp detalles del complejo

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,34 @@
-import './App.css';
-import Landing from './components/Landing';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import TeamInscription from './components/TeamInscription';
-import PlayerInscription from './components/PlayerInscription';
-import CreateTournament from './components/CreateTournament';
+import "./App.css";
+import Landing from "./components/Landing";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import TeamInscription from "./components/TeamInscription";
+import PlayerInscription from "./components/PlayerInscription";
+import CreateTournament from "./components/CreateTournament";
 
-import Error404 from './components/Error404';
+import Error404 from "./components/Error404";
+import ComplexDetail from "./components/ComplexDetail";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<div>
-				<Switch>
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/home" component={Home} />
-					<Route exact path="/inscription" component={TeamInscription} />
-					<Route
-						exact
-						path="/inscription/players"
-						component={PlayerInscription}
-					/>
-					<Route exact path="/create" component={CreateTournament} />
-					<Route exact path="/404" component={Error404} />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/inscription" component={TeamInscription} />
+          <Route
+            exact
+            path="/inscription/players"
+            component={PlayerInscription}
+          />
+          <Route exact path="/create" component={CreateTournament} />
+          <Route exact path="/404" component={Error404} />
+          <Route exact path="/complex" component={ComplexDetail} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
