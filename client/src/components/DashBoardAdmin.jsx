9d1fae3a776eb397b5259866c72a2b7661776050
Matias Franco <matias.franco289@gmail.com>
2022-09-25 12:01:56 -0300

    Agregados estilos temporales a /admin

--- client/src/components/DashBoardAdmin.jsx
+++ client/src/components/DashBoardAdmin.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
+import Nav from './Nav';
+import Footer from './Footer';
 
 import {
 	getAllUsers,
@@ -53,13 +55,21 @@ export default function DashBoardAdmin() {
 	//admin de torneos: Eliminar, modificar, crear
 	//admin de reseñas: eliminar
 	return (
-		<div>
-			<h1>Panel de administrador</h1>
-			<div>
-				<h2>Administracion de usuarios: </h2>
+		<div className='min-h-screen flex flex-col justify-between items-center'>
+			<Nav/>
+
+			<div className='bg-gray-100 flex flex-col items-center p-10 shadow shadow-black'>
+			<h1 className='text-2xl font-bold'>Panel de administrador</h1>
+
+			<div className='p-3 flex flex-col items-center'>
+				<h2 className='text-xl font-medium'>Administracion de usuarios: </h2>
 				<div>
-					<h3>Banear Usuario:</h3>
-					<select onChange={(e) => handleSelectUser(e)}>
+					<h3 className='text-lg font-medium'>Banear Usuario:</h3>
+					<select 
+					onChange={(e) => handleSelectUser(e)}
+					className='bg-green-300 p-2 rounded font-medium 
+					cursor-pointer hover:scale-110 duration-300 hover:text-white'
+					>
 						<option>Ver usuarios</option>
 						{allUsers?.map((e) => (
 							<option key={e.id} value={e.id}>
@@ -68,15 +78,19 @@ export default function DashBoardAdmin() {
 						))}
 					</select>
 					{idUser === '' ? (
-						<p>Seleccione un usuario</p>
+						<p className='text-red-700'>Seleccione un usuario</p>
 					) : (
 						<button onClick={(e) => handleBan(e)}>Banear user</button>
 					)}
 				</div>
 			</div>
-			<div>
-				<h2>Administracion de torneos:</h2>
-				<select onChange={(e) => handleSelectTournament(e)}>
+			<div className='flex flex-col items-center'>
+				<h2 className='text-xl font-medium'>Administracion de torneos:</h2>
+				<select onChange={(e) => 
+					handleSelectTournament(e)}
+					className='bg-green-300 p-2 rounded font-medium cursor-pointer
+					hover:scale-110 duration-300 hover:text-white'
+					>
 					<option>Ver torneos</option>
 					{allTournaments?.map((e) => (
 						<option key={e.id} value={e.id} name={e.name}>
@@ -85,7 +99,7 @@ export default function DashBoardAdmin() {
 					))}
 				</select>
 				{idTournament === '' ? (
-					<p>Seleccione un torneo</p>
+					<p className='text-red-700'>Seleccione un torneo</p>
 				) : (
 					<Link to={`/admin/modify/${idTournament}`}>
 						<button>Modifica el torneo</button>
@@ -100,12 +114,18 @@ export default function DashBoardAdmin() {
 				)}
 
 				<Link to="/create">
-					<button>Crear nuevo torneo</button>
+					<button
+					className='bg-green-300 font-medium p-3 rounded-full
+					hover:scale-110 hover:text-white duration-300'
+					>Crear nuevo torneo</button>
 				</Link>
 			</div>
 			<div>
-				<h2>Administacion de reseñas</h2>
+				<h2 className='text-medium'>Administacion de reseñas</h2>
 			</div>
+		</div>
+
+		<Footer/>
 		</div>
 	);
 }
