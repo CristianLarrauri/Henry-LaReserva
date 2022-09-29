eeb1367ffe1a14f26f09e931dd946c4760f5295f
Matias Franco <matias.franco289@gmail.com>
2022-09-29 04:46:54 -0300

    Finalizados los estilos de complex

--- client/src/components/ComplexDetail.jsx
+++ client/src/components/ComplexDetail.jsx
@@ -5,102 +5,112 @@ import Footer from "./Footer";
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 import Birra from "../images/Birra.png"
-import Cancha from "../images/Captura.PNG"
-import Vestuario from "../images/Vestuario.png"
+import Cancha1 from "../images/Cancha001.jpg"
+import Cancha2 from "../images/Cancha002.jpg"
+import Cancha3 from "../images/Cancha003.jpg"
+import Brindis1 from "../images/Brindis_001.jpg"
+import Brindis2 from "../images/Brindis_002.jpg"
+import Brindis3 from "../images/Brindis_003.jpg"
+import Vestuario1 from "../images/Vestuario_001.jpg"
+import Vestuario2 from "../images/Vestuario_002.jpg"
+import Vestuario3 from "../images/Vestuario_003.jpg"
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 export default function ComplexDetail() {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if(offset > 800){setOffset(0)}else{setOffset(offset+800)}
+    },3000);
+  },[offset])
+
+
   return (
-    <div>
-      {/* <Nav /> */}
-      <div className="items-center justify-center flex flex-col z-10">
-        <Link to="/home" >
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold mt-[3%] py-2 px-4 rounded-lg ml-5 text-center mb-[-4%]">
-            <p>Home</p>
-            <p>{"<<"}</p>
-        </button>
-        </Link>
-        <img src={Logo} alt="" className="img-fluid w-[18%] h-[1%] mt-[-2%]" />
+    <div className="w-full min-h-screen flex flex-col justify-between items-center bg-white text-center">
+      <Nav/>
+      <div className=" w-full text-center">
+        <h1 className="text-gray-700 text-3xl font-bold mt-6">Detalles del complejo</h1>
       </div>
 
+      <div className="w-full flex flex-col justify-around items-center
+      2xl:flex-row mt-8">
+        
+        {/*Vestuario/Brindis wrapper*/}
+        <div className="flex w-9/12 justify-around flex-col-reverse
+        2xl:flex-row items-center text-left">
+          {/*Brindis section*/}
+          <div className="max-w-[320px] p-3 m-3">
+            <p className="text-gray-700 text-lg">
+              Nuestra vision a largo plazo como empresa dedicada a eventos deportivos
+              es ser lideres en diseño, produccion, logistica y supervision en eventos
+              sociales y deportivos en La Republica Argentina, ofreciendo a los
+              miembros de nuestra comunidad la posibilidad de participar en una amplia
+              gama de competiciones y actividades relacionadas al deporte rey sin
+              hacer distinción por sus capacidades, edad o sexo.
+            </p>
 
-      {/* CONTAINER MAYOR */}
-      <div className="flex container-xl items-center w-full mt-[-5%] mb-5 border-4 justify-between max-h-full z-20">
-
-
-        {/* CONTAINER VISION */}
-        <div className="w-[20%] px-[2%] mb-5 mt-1 py-[3%] items-center flex flex-col container mx-5 border-lime-400 border-2 rounded-lg">
-          <h3 className="text-2xl font-mono text-center underline mb-3 mt-5  hover:underline-offset-4">Vision:</h3>
-          <p className="text-md font-mono text-center">
-            Nuestra vision a largo plazo como empresa dedicada a eventos deportivos
-            es ser lideres en diseño, produccion, logistica y supervision en eventos
-            sociales y deportivos en La Republica Argentina, ofreciendo a los
-            miembros de nuestra comunidad la posibilidad de participar en una amplia
-            gama de competiciones y actividades relacionadas al deporte rey sin
-            hacer distinción por sus capacidades, edad o sexo.
-          </p>
-          <div className="mt-5">
-            <img src={Birra} alt="fotodelcomplejo" className="mb-1 rounded-lg" />
-            {/* reemplazar logo por fotos de vestuario/duchas y alguna cancha */}
-          </div>
-        </div>
-
-        {/* CONTAINER QUIENES SOMOS? */}
-        <div className="mb-[1%] px-[5%] py-[2%] items-center justify-between w-[70%] mx-[3%] border-lime-400 border-2 rounded-lg">
-          <div className="px-[5%] ml-3 rounded-lg">
-            <img src={Cancha} alt="fotodelcomplejo" className="rounded-lg" />
-            {/* reemplazar logo por fotos de vestuario/duchas y alguna cancha */}
+            
+            <div className="w-[300px] overflow-hidden mt-6">
+              <div className={`flex translate-x-[-${offset/2-offset/8}px] duration-300`}>
+                <img src={Brindis1} alt='Brindis.jpg' />
+                <img src={Brindis2} alt='Brindis.jpg' />
+                <img src={Brindis3} alt='Brindis.jpg' />
+              </div>
+            </div>
           </div>
 
-          <h1 className="text-2xl font-mono text-center underline mb-3 hover:underline-offset-4">¿Quienes somos?</h1>
+          {/*middle section*/}
+          <div className="max-w-[820px] p-3 m-3 flex flex-col items-center">
+            
+            <div className="w-[300px] h-[400px] flex overflow-hidden lg:w-[800px]
+            sm:w-[600px]">
+              <div className={`flex translate-x-[-${offset}px] duration-300`}>
+                <img className="min-w-[800px] h-full" src={Cancha1} alt="cancha.png" />
+                <img className="min-w-[800px] h-full" src={Cancha2} alt="cancha.png" />
+                <img className="min-w-[800px] h-full" src={Cancha3} alt="cancha.png" />
+              </div>
+            </div>
 
-          <p className="text-md font-mono text-center">
-            <a className="underline decoration-lime-600">La Reserva®</a> es un predio deportivo con canchas de fútbol 7
-            alfombrado con el mejor cesped sintetico del mercado compuesto de fibra
-            de polietileno con tratamiento UV. Contamos con 2 Vestuarios de
-            capacidad para 60 personas con 20 duchas y servicio de Guardaropa,
-            ademas poseemos un amplio Bar donde podras compartir una bebida o comida
-            con tu equipo o rivales. En <a className="underline decoration-lime-600">La Reserva®</a> queremos incentivar
-            el tercer tiempo y generar camaraderia entre contrincantes y compañeros
-            de equipo. Nuestras competiciones se llevan a cabo dentro de un marco
-            distendido y familiar logrando que todos los integrantes se diviertan y
-            a la vez participen de un torneo competitivo. Nuestra finalidad ademas
-            de ser un proyecto serio y con grandes expectativas es que nuestros
-            participantes compartan nuestros valores y continua necesidad de
-            superacion, se vive como se juega!
-          </p>
-        </div>
+            <p className="text-gray-700 text-lg mt-3">
+              <a>La Reserva®</a> es un predio deportivo con canchas de fútbol 7
+              alfombrado con el mejor cesped sintetico del mercado compuesto de fibra
+              de polietileno con tratamiento UV. Contamos con 2 Vestuarios de
+              capacidad para 60 personas con 20 duchas y servicio de Guardaropa,
+              ademas poseemos un amplio Bar donde podras compartir una bebida o comida
+              con tu equipo o rivales. En <a className="underline decoration-lime-600">La Reserva®</a> queremos incentivar
+              el tercer tiempo y generar camaraderia entre contrincantes y compañeros
+              de equipo. Nuestras competiciones se llevan a cabo dentro de un marco
+              distendido y familiar logrando que todos los integrantes se diviertan y
+              a la vez participen de un torneo competitivo. Nuestra finalidad ademas
+              de ser un proyecto serio y con grandes expectativas es que nuestros
+              participantes compartan nuestros valores y continua necesidad de
+              superacion, se vive como se juega!
+            </p>
 
-        {/* CONTAINER LISTA */}
-        <div className="w-[20%] text-center font-mono items-center flex flex-col container border-lime-400 border-2 rounded-lg h-[50%] py-[60px]">
-          <ul className="flex flex-col justify-between text-xl">
-            <li>
-              <span>-<a className="underline decoration-lime-600">COMPROMISO</a></span>
-            </li>
-            <span>-<a className="underline decoration-lime-600">HONESTIDAD</a></span>
-            <li>
-              <span>-<a className="underline decoration-lime-600">CALIDAD</a></span>
-            </li>
-            <li>
-              <span>-<a className="underline decoration-lime-600">INNOVACION</a></span>
-            </li>
-          </ul>
-          <div>
-            <img src={Vestuario} alt="fotodelcomplejo" className="rounded-lg w-full h-[50%] " />
-            {/* reemplazar logo por fotos de vestuario/duchas y alguna cancha */}
           </div>
         </div>
+        
+        {/*Vestuario section*/}
+        <div>
 
+        </div>
+          <div className="bg-blue-300 w-[200px] h-[600px] overflow-hidden m-3">
+            <div className={`flex w-[200px] h-full translate-x-[-${offset/4}px] duration-300`}>
+              <img className="w-full h-full" src={Vestuario1} alt="vestuario.png" />
+              <img className="w-full h-full" src={Vestuario2} alt="vestuario.png" />
+              <img className="w-full h-full" src={Vestuario3} alt="vestuario.png" />
+            </div>
+          </div>
 
       </div>
 
-
-
-
-      <div className="w-full container-xl">
-        <Footer />
-      </div>
-
+      <Footer/>
     </div>
   );
 }
+
+
+
